a83c322d4e1b06493810c37778885ec58a6271b2
victor sales <sales.victor2304@gmail.com>
2021-11-07 19:11:30 -0300

    wip

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port",System.getenv().getOrDefault("PORT","8080")));
+		app.run(args);
+		//SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
